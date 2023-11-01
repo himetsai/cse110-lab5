cf8bb839b2f696fed7096fdf349750db5d0d8d24
SRaswan <shauryaraswan@gmail.com>
2023-10-31 18:29:54 -0700

    Delete handle

--- src/server/RequestHandler.java
+++ src/server/RequestHandler.java
@@ -42,8 +42,8 @@ public class RequestHandler implements HttpHandler {
         URI uri = httpExchange.getRequestURI();
         String query = uri.getRawQuery();
         if (query != null) {
-                String value = query.substring(query.indexOf("=") + 1);
-                String year = data.get(value); // Retrieve data from hashmap
+            String value = query.substring(query.indexOf("=") + 1);
+            String year = data.get(value); // Retrieve data from hashmap
             if (year != null) {
                 response = year;
                 System.out.println("Queried for " + value + " and found " + year);
@@ -76,6 +76,22 @@ public class RequestHandler implements HttpHandler {
         return response;
     }
 
+    private String handleDelete(HttpExchange httpExchange) throws IOException {
+        String response = "Invalid GET request";
+        URI uri = httpExchange.getRequestURI();
+        String query = uri.getRawQuery();
+        if (query != null) {
+            String value = query.substring(query.indexOf("=") + 1);
+            String year = data.get(value); // Retrieve data from hashmap
+            if (year != null) {
+                data.remove(value);
+                response = "Deleted entry {" + value + ", " + year + "}";
+            } else {
+                response = "No data found for " + value;
+            }
+        }
+        return response;
+    }
 
 
 
